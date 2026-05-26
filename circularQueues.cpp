f545d89dd3b2849715073c6b62ee62af1fb96065
nanayyayaa <jiejueejuju@gmail.com>
2026-05-26 11:30:45 +0700

    commit 10 new

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -99,3 +99,12 @@ public:
                 cout << queue_array[FRONT_position] << "  ";
                 FRONT_position++;
             }
+
+            FRONT_position = 0;
+
+            //iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
