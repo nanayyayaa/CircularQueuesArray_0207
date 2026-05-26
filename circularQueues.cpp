3eb329eb2c73239044ef699c3eed3a0ca168fee8
nanayyayaa <jiejueejuju@gmail.com>
2026-05-26 11:24:40 +0700

    commit 2 new

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -8,3 +8,13 @@ private:
     int queue_array[max];
 
 public:
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert () {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
